137497fdf5ed4eadd8a29bc21c7ee010557da7d4
Roger Espinoza <rj.espinoza44@gmail.com>
2022-12-12 21:42:58 -0500

    Initial Commit

---
+++ RogerCard/RogerCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RogerCardApp.swift
+//  RogerCard
+//
+//  Created by Roger Espinoza on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RogerCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
